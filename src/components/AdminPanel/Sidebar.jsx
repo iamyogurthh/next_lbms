16ba85383d1b0a1ed3d46a0b1d2aaca595260ad7
iamyogurthh <saieainkhant992003@gmail.com>
2024-12-26 19:18:43 +0630

    more-interactive-sidebar

--- src/components/AdminPanel/Sidebar.jsx
+++ src/components/AdminPanel/Sidebar.jsx
@@ -1,7 +1,17 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+  const pathName = usePathname()
+
+  const listItems = [
+    { path: '/adminPanel/addbook', label: 'Add Book' },
+    { path: '/adminPanel/users', label: 'Users' },
+    { path: '/adminPanel/editbook', label: 'Edit Book' },
+  ]
+
   return (
     <div className="max-w-[396px] bg-[#F7F2FA] flex flex-col fixed bottom-0 top-0">
       <div className="p-8">
@@ -9,15 +19,16 @@ const Sidebar = () => {
         <span className="font-bold text-[24px]">Admin-Dashboard</span>
       </div>
       <ul className="px-[17px]">
-        <li className="w-[336px] h-[56px]  flex justify-center items-center text-[14px] font-bold active">
-          <Link href={'/adminPanel/addbook'}>Add Book</Link>
-        </li>
-        <li className="w-[336px] h-[56px]  flex justify-center items-center text-[14px] font-bold">
-          <Link href={'/adminPanel/users'}>Users</Link>
-        </li>
-        <li className="w-[336px] h-[56px]  flex justify-center items-center text-[14px] font-bold ">
-          <Link href={'/adminPanel/editbook'}>Edit Book</Link>
-        </li>
+        {listItems.map((item, index) => (
+          <li
+            key={index}
+            className={`sidebar_items ${
+              pathName === item.path ? 'active' : ''
+            }`}
+          >
+            <Link href={item.path}>{item.label}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
