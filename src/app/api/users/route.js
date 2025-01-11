ab2ad3b0105dacc40d1b8882f2ce8eebae4b7e4f
iamyogurthh <saieainkhant992003@gmail.com>
2025-01-11 10:49:39 +0630

    erros-to-fix

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,14 +1,17 @@
-import { connectdb, getDataFromForm } from "@/libs/utils";
-import User from "@/models/User";
-import { NextResponse } from "next/server";
+import { connectdb, getDataFromForm } from '@/libs/utils'
+import User from '@/models/User'
+import { NextResponse } from 'next/server'
 
 export async function GET(request) {
-    try {
-        await connectdb();
-        const users = await User.find().select('-password');
-        return NextResponse.json(users, { status: 200 });
-    } catch (error) {
-        console.log(error.message);
-        return NextResponse.json({ message: "An error occurred while processing the request." }, { status: 500 });
-    }
+  try {
+    await connectdb()
+    const users = await User.find().select('-password')
+    return NextResponse.json(users, { status: 200 })
+  } catch (error) {
+    console.log(error.message)
+    return NextResponse.json(
+      { message: 'An error occurred while processing the request.' },
+      { status: 500 }
+    )
+  }
 }
